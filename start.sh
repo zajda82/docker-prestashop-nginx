a4779391196e0349b100010e5a201cf7c1d2afeb
Zajda82 <zajda82@gmail.com>
2016-03-22 13:15:56 +0100

    changed sed delimiter for cert key path

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 [ ! -z "${PHP_HOST}" ] && sed -i "s/PHP_HOST/${PHP_HOST}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${PHP_PORT}" ] && sed -i "s/PHP_PORT/${PHP_PORT}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${CERTIFICATE_PATH}" ] && sed -i "s@CERTIFICATE_PATH@${CERTIFICATE_PATH}@" /etc/nginx/conf.d/default.conf
-[ ! -z "${CERTIFICATE_KEY_PATH}" ] && sed -i "s/CERTIFICATE_KEY_PATH/${CERTIFICATE_KEY_PATH}/" /etc/nginx/conf.d/default.conf
+[ ! -z "${CERTIFICATE_KEY_PATH}" ] && sed -i "s@CERTIFICATE_KEY_PATH@${CERTIFICATE_KEY_PATH}@" /etc/nginx/conf.d/default.conf
 
 /usr/sbin/nginx -g "daemon off;"
