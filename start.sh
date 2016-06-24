d1a6b2273e00c912fc458bbae0796c0133db0997
U-HCG\peter.zajic <peter.zajic@N0711.hcg.homecredit.net>
2016-06-24 15:35:52 +0200

    removed SSH from default configuration

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 [ ! -z "${VIRTUAL_HOST}" ] && sed -i "s/VIRTUAL_HOST/${VIRTUAL_HOST}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${PHP_HOST}" ] && sed -i "s/PHP_HOST/${PHP_HOST}/" /etc/nginx/conf.d/default.conf
 [ ! -z "${PHP_PORT}" ] && sed -i "s/PHP_PORT/${PHP_PORT}/" /etc/nginx/conf.d/default.conf
-[ ! -z "${CERTIFICATE_PATH}" ] && sed -i "s@CERTIFICATE_PATH@${CERTIFICATE_PATH}@" /etc/nginx/conf.d/default.conf
-[ ! -z "${CERTIFICATE_KEY_PATH}" ] && sed -i "s@CERTIFICATE_KEY_PATH@${CERTIFICATE_KEY_PATH}@" /etc/nginx/conf.d/default.conf
+#[ ! -z "${CERTIFICATE_PATH}" ] && sed -i "s@CERTIFICATE_PATH@${CERTIFICATE_PATH}@" /etc/nginx/conf.d/default.conf
+#[ ! -z "${CERTIFICATE_KEY_PATH}" ] && sed -i "s@CERTIFICATE_KEY_PATH@${CERTIFICATE_KEY_PATH}@" /etc/nginx/conf.d/default.conf
 
 /usr/sbin/nginx -g "daemon off;"
